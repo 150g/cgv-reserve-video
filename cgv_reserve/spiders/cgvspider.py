1373c89ba3b545e8cf5c75c346e376380bd95f91
EngHyu <roomedia@naver.com>
2019-05-26 14:51:58 +0900

    now crawl theaters, movies, seats but be sure to make interval

--- cgv_reserve/spiders/cgvspider.py
+++ cgv_reserve/spiders/cgvspider.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# need to make interval
+# if you just dismiss it, your ip will be banned
 import re
 import json
 
@@ -6,41 +8,64 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 
 class CGVSpider(scrapy.Spider):
-    name = 'cgvspider'
-    allowed_domains = ['www.cgv.co.kr']
-    start_urls = ['http://www.cgv.co.kr/']
-    getSeatList = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx/GetSeatList'
-    header = { 'Content-Type': 'application/json' }
+    name         = 'cgvspider'
+    theaterUrl   = 'http://www.cgv.co.kr/theaters/'
+    timetableUrl = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx/GetSeatList'
+    header       = { 'Content-Type': 'application/json' }
 
     def start_requests(self):
-        code = '0001'
-        PlayYMD = '20190428'
-        ScreenCode = '003'
-        PlayNum = '6'
-        StartTime = '2530'
-        EndTime = '2841'
-        TheaterName = 'CGV 강변'
-        Cnt = '202'
-        ScreenName = '3관'
+        yield scrapy.Request(url=self.theaterUrl, callback=self.getTheater)
 
-        payload = json.dumps({
-            'theatercode': code,
-            'palyymd': PlayYMD,
-            'screencode': ScreenCode,
-            'playnum': PlayNum,
-            'starttime': StartTime,
-            'endtime': EndTime,
-            'theatername': TheaterName,
-            'cnt': Cnt,
-            'screenname': ScreenName
-        })
+    def getTheater(self, response):
+        html = response.body_as_unicode()
+        dict = re.search( r"var theaterJsonData = (.*);", html ).group().split('= ')[1].split(';')[0]
 
-        with open('theaterCode.json') as json_file:
-            self.theater = json.load(json_file)
+        from functools import reduce
+        theater = map( lambda i: i['AreaTheaterDetailList'], json.loads( dict ) )
+        self.theater = reduce( lambda x, y: x + y, theater )
 
-        yield scrapy.FormRequest(url=self.getSeatList, method="POST", body=payload, headers=self.header, callback=self.parse)
+        for i in self.theater:
+            print(i)
+            self.theaterCode = i['TheaterCode']
+            self.theaterName = i['TheaterName']
+            yield scrapy.Request(
+                url='%s?%s=%s' % ( self.timetableUrl, 'theaterCode', self.theaterCode ),
+                callback=self.getTimetable
+            )
+            break
 
-    def parse(self, response):
+    def getTimetable(self, response):
+        """
+        from collections import defaultdict
+        self.movies = defaultdict(lambda: {})
+
+        for movie in response.css( '.sect-showtimes > ul > li' ):
+            title = movie.css('strong::text').get().strip()
+        """
+
+        for time in response.css('.info-timetable a'):
+            payload = json.dumps({
+                'theatercode': self.theaterCode,
+                'theatername': self.theaterName,
+                'palyymd':     time.attrib['data-playymd'],
+                'screencode':  time.attrib['data-screencode'],
+                'playnum':     time.attrib['data-playnum'],
+                'starttime':   time.attrib['data-playstarttime'],
+                'endtime':     time.attrib['data-playendtime'],
+                'cnt':         time.attrib['data-seatremaincnt'],
+                'screenname':  time.attrib['data-screenkorname']
+            });
+
+            yield scrapy.FormRequest(
+                url=self.timetableUrl,
+                method="POST",
+                body=payload,
+                headers=self.header,
+                callback=self.getSeats
+            )
+            break
+
+    def getSeats(self, response):
         text = response.body_as_unicode()
         text = json.loads( text )['d']
         coordinates = re.findall(r"(reserved)?.{13}left:([0-9]{1,3})px; top:([0-9]{1,3})px;", text)
